0fee5a15ee59dcf07e4a2b066b54311064ce844b
Fernando <fernandoj.email@gmail.com>
2025-03-16 13:45:57 -0300

    conclusão - primeiros passos com flutter

--- curso_alura/lib/main.dart
+++ curso_alura/lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text('Bem vindo ao Bytebank', textDirection: TextDirection.ltr,));
+  runApp(
+    const Column(
+  children: <Widget>[
+    Text('texto 1', textDirection: TextDirection.ltr,),
+    Text('texto 2', textDirection: TextDirection.ltr,),
+    Text('texto 3', textDirection: TextDirection.ltr,),
+    const Column(
+      children: <Widget>[
+        Text('texto 4 - dentro de outra coluna', textDirection: TextDirection.rtl,),
+      ],),
+    Expanded(
+      child: FittedBox(
+        child: FlutterLogo(),
+      ),
+    ),
+  ],
+)
+  );
 }
 
