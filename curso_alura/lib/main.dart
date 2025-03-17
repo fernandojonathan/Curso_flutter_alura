c31080b6905b33c11e48b45d6bd3b8605f77426b
Fernando <fernandoj.email@gmail.com>
2025-03-17 20:15:56 -0300

    Refatorando o código

--- curso_alura/lib/main.dart
+++ curso_alura/lib/main.dart
@@ -1,33 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() { runApp(MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: Text('Transferências'),),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {
-            },
-          ),
-          body: Column(
-            children: <Widget>[
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on_rounded),
-                  title: Text('100.00'),
-                  subtitle: Text('1000.00'),
-                )
-                ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on_rounded),
-                  title: Text('200.00'),
-                  subtitle: Text('2000.00'),
-                )
-                ),
-            ]
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: 
+        
+        ListaTransferencias(),
+      
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text('Transferências'),
       ),
-    )
-  )
- );
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),      
+    ),
+  ));
 }
 
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.0,1000)),
+        ItemTransferencia(Transferencia(200.0,2000)),
+        ItemTransferencia(Transferencia(300.0,3000)),
+      
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  
+  final Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+        return Card(
+          child: ListTile(
+            leading: Icon(Icons.monetization_on_rounded),
+            title: Text(_transferencia.valor.toString()),
+            subtitle: Text(_transferencia.numeroConta.toString()),
+          ),
+        );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
