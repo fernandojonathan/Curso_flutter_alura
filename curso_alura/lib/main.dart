c9b1cf98c4b204c86021b52e6f31024ba759cc1c
Fernando <fernandoj.email@gmail.com>
2025-03-17 21:30:16 -0300

    conhecendo o hotreload

--- curso_alura/lib/main.dart
+++ curso_alura/lib/main.dart
@@ -1,59 +1,68 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: 
-        
-        ListaTransferencias(),
-      
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: FormularioTransferencia()));
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
-        title: Text('Transferências'),
+        title: Text('Criando Transferências'),
+        backgroundColor: Colors.deepPurpleAccent,
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
-      ),      
-    ),
-  ));
+      body: Text('teste'),
+    );
+  }
 }
 
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(Transferencia(100.0,1000)),
-        ItemTransferencia(Transferencia(200.0,2000)),
-        ItemTransferencia(Transferencia(300.0,3000)),
-      
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(Transferencia(100.0, 1000)),
+          ItemTransferencia(Transferencia(200.0, 2000)),
+          ItemTransferencia(Transferencia(300.0, 3000)),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
-  
   final Transferencia _transferencia;
-
   ItemTransferencia(this._transferencia);
 
   @override
   Widget build(BuildContext context) {
-        return Card(
-          child: ListTile(
-            leading: Icon(Icons.monetization_on_rounded),
-            title: Text(_transferencia.valor.toString()),
-            subtitle: Text(_transferencia.numeroConta.toString()),
-          ),
-        );
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on_rounded),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
   }
 }
 
 class Transferencia {
   final double valor;
   final int numeroConta;
-
   Transferencia(this.valor, this.numeroConta);
 }
